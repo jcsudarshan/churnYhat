232dfb6ee5e76c7f9f38aadc121f222f32a0cb92
upjohnc <upjohnc@gmail.colm>
2014-04-07 17:47:11 -0500

    Replicated Example Code - Ready to deploy to that

---
+++ churnDeployment.py
@@ -0,0 +1,32 @@
+from yhat import Yhat,YhatModel,preprocess
+import pandas as pd
+
+class ChurnModel(YhatModel):
+    # Type casts incoming data as a dataframe
+    @preprocess(in_type=pd.DataFrame,out_type=pd.DataFrame)
+    def execute(self,data):
+        # Collect customer meta data
+        response = data[['Area Code','Phone']]
+        charges = ['Day Charge','Eve Charge','Night Charge','Intl Charge']
+        response['customer_worth'] = data[charges].sum(axis=1)
+        # Convert yes no columns to bool
+        data[yes_no_cols] = data[yes_no_cols] == 'yes'
+        # Create feature space
+        X = data[features].as_matrix().astype(float)
+        X = scaler.transform(X)
+        # Make prediction
+        churn_prob = clf.predict_proba(X)
+        response['churn_prob'] = churn_prob[:,1]
+        # Calculate expected loss by churn
+        response['expected_loss'] = response['churn_prob'] * response['customer_worth']
+        response = response.sort('expected_loss', ascending=False)
+        # Return response DataFrame
+        return response
+
+yh = Yhat(
+    "upjohnc@gmail.com",
+    "eb27a1a0836fea54ffa7c994917e5c92",
+    "http://cloud.yhathq.com/"
+)
+
+response = yh.deploy("PythonChurnModel",ChurnModel,globals())
